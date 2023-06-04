00e6e6768ca40fe9412981083be023415cf4ca70
MalsmtenTheo <theo.m1590@gmail.com>
2023-06-04 20:45:42 -0300

    Correccion al if

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 
-const bienvenido = prompt("Bienvenido! Que accion desea realizar? \n" + "1- Agregar stock \n" + "2- Quitar stock") 
+/* const bienvenido = prompt("Bienvenido! Que accion desea realizar? \n" + "1- Agregar stock \n" + "2- Quitar stock")  */
 
-if (bienvenido == 1) {
-    const queCantidad = [prompt("Cuantos productos desea agregar: ")]
+/* if (bienvenido == 1) { */
+    /* const queCantidad = [prompt("Cuantos productos desea agregar: ")] */
     class stock {
         constructor(productos){
             this.productos = productos
         }
         agregarStock(){
+            const bienvenido = prompt("Bienvenido! Que accion desea realizar? \n" + "1- Agregar stock \n" + "2- Quitar stock") 
+            const queCantidad = [prompt("Cuantos productos desea agregar: ")]
             for (let index = 0; index < queCantidad; index++) {
             productos.push(prompt("Ingrese el producto que desee agregar al stock: "))
             productos.push(prompt("Ingrese la cantidad del mismo: "))
@@ -15,17 +17,18 @@ if (bienvenido == 1) {
         }
     }
     this.productos = []
-    
     const cliente1 = new stock().agregarStock()
-    
-    const productosLista = productos.join(" - ")
-    alert("Usted sumara al stock: \n" + productosLista)
-    
-}else if(bienvenido == 2){
+    let productosLista1 = productos.join(" - ")
+    alert("Stock: \n" + productosLista1)
+
+    const cliente2 = new stock().agregarStock()
+    let productosLista2 = productos.join(" - ")
+    alert("Stock: \n" + productosLista2)
+/* }else if(bienvenido == 2){
     alert("En proceso!")
 } else if (bienvenido == null){
     alert("Gracias por ingresar!")
 }else{
     alert("Ingrese una de las opciones validas!")
 }  
-
+ */
