8816680c3060aeb0286978e2bcc121b240b7db94
MalsmtenTheo <theo.m1590@gmail.com>
2023-06-05 21:27:13 -0300

    Finalizado

--- js/main.js
+++ js/main.js
@@ -1,34 +1,42 @@
+let bienvenido = prompt("Bienvenido! Ingrese: \n" + "1- Agregar stock")
 
-/* const bienvenido = prompt("Bienvenido! Que accion desea realizar? \n" + "1- Agregar stock \n" + "2- Quitar stock")  */
-
-/* if (bienvenido == 1) { */
-    /* const queCantidad = [prompt("Cuantos productos desea agregar: ")] */
+if (bienvenido == 1) { 
     class stock {
-        constructor(productos){
+        constructor(productos, cantidad){
             this.productos = productos
+            this.cantidad = cantidad 
         }
         agregarStock(){
-            const bienvenido = prompt("Bienvenido! Que accion desea realizar? \n" + "1- Agregar stock \n" + "2- Quitar stock") 
             const queCantidad = [prompt("Cuantos productos desea agregar: ")]
-            for (let index = 0; index < queCantidad; index++) {
-            productos.push(prompt("Ingrese el producto que desee agregar al stock: "))
-            productos.push(prompt("Ingrese la cantidad del mismo: "))
+            for (let index1 = 0; index1 < queCantidad; index1++) {
+            productos.push(prompt("Ingrese el producto que desee agregar al stock: ").toUpperCase())
+            cantidad.push(parseInt(prompt("Ingrese la cantidad del mismo: ")))
             }
         }
     }
     this.productos = []
+    this.cantidad = []
+    
+    
     const cliente1 = new stock().agregarStock()
-    let productosLista1 = productos.join(" - ")
-    alert("Stock: \n" + productosLista1)
-
-    const cliente2 = new stock().agregarStock()
-    let productosLista2 = productos.join(" - ")
-    alert("Stock: \n" + productosLista2)
-/* }else if(bienvenido == 2){
-    alert("En proceso!")
+    
+    let salida = ""
+    
+    /* for (let index = 0; index < cantidad.length; index++) {
+        salida += "Stock del producto " + productos[index] + ": " + cantidad[index] + "\n"; 
+    }*/
+    
+    
+    cantidad.forEach(function a (cantidad, i){
+        salida += "Stock del producto " + this.productos[i] + ": " + cantidad + "\n";
+    })
+    
+    alert(salida) 
+    
+    
 } else if (bienvenido == null){
     alert("Gracias por ingresar!")
 }else{
     alert("Ingrese una de las opciones validas!")
-}  
- */
+}   
+
