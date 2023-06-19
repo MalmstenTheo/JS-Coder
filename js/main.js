92d8a7a44e7c977d6b5b4da52847ce0ca646949d
MalsmtenTheo <theo.m1590@gmail.com>
2023-06-18 21:15:35 -0300

    Tercera pre-entrega

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let bienvenido = prompt("Bienvenido! Ingrese: \n" + "1- Agregar stock")
+/* let bienvenido = prompt("Bienvenido! Ingrese: \n" + "1- Agregar stock")
 
 if (bienvenido == 1) { 
     class stock {
@@ -22,9 +22,9 @@ if (bienvenido == 1) {
     
     let salida = ""
     
-    /* for (let index = 0; index < cantidad.length; index++) {
+    for (let index = 0; index < cantidad.length; index++) {
         salida += "Stock del producto " + productos[index] + ": " + cantidad[index] + "\n"; 
-    }*/
+    }
     
     
     cantidad.forEach(function a (cantidad, i){
@@ -38,5 +38,60 @@ if (bienvenido == 1) {
     alert("Gracias por ingresar!")
 }else{
     alert("Ingrese una de las opciones validas!")
-}   
+}   */   
+
+
+function renderizarProducto(producto) {
+    let lista = document.querySelector('#listadoProductos')
+    let divProducto = document.createElement("div")
+    divProducto.classList.add("producto")
+    divProducto.innerHTML = `<li>${producto}</li>`
+    lista.append(divProducto)
+}
+
+var numeros="0123456789";
+
+function tiene_numeros(texto){
+    for(i=0; i<texto.length; i++){
+        if (numeros.indexOf(texto.charAt(i),0)!=-1){
+            return true;
+        }
+    }
+    return false;
+}
+
+const productos = JSON.parse(localStorage.getItem('productos')) || []
+
+let error
+
+function mostrarInput() {
+
+    let inputProducto = document.getElementById("1").value
+    let alerta =  document.getElementById("alerta")
+    if(tiene_numeros(inputProducto)){
+        alerta.innerHTML = "<p class='alertaTexto'> SOLO SE PUEDEN INGRESAR LETRAS </p>"
+        return
+    } else {
+        alerta.innerHTML = ''
+        productos.push(inputProducto)
+        localStorage.setItem("productos", JSON.stringify(productos))
+        renderizarProducto(inputProducto)
+    }
+}
+
+if(productos.length > 0){
+    for (const producto of productos) {
+        renderizarProducto(producto)
+    }
+} 
+
+function vaciarProductos () {
+    let lista = document.getElementById('listadoProductos')
+    lista.innerHTML = ''
+    for (let index = 0; index < productos.length; index++) {
+        productos.pop()
+    }
+    localStorage.clear()
+}
+
 
